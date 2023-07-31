fbc4668dc6a73b20206a24c78167abf1dca825c5
Caleb Nkunze <nkunzecaleb@gmail.com>
2023-07-31 15:11:53 +0300

    Add border on SingleArtist and SingleAlbum views

--- resources/js/Pages/SingleAlbum.vue
+++ resources/js/Pages/SingleAlbum.vue
@@ -43,7 +43,7 @@ const showLessSimilarAlbums = () => {
 
         <div class="max-w-7xl mx-auto lg:px-8 space-y-6">
             <div class="w-[78.58vw] h-[91vh] overflow-x-hidden mt-12 ml-[7.5rem] shadow rounded-b-xl items-center justify-between bg-white dark:bg-gray-900">
-                <div class="pr-8 pl-8 pt-6">
+                <div class="pr-8 pl-8 pt-6 border-2 border-t-0 border-gray-800">
                     <div class="p-8 overflow-x-hidden">
 
                         <div class="py-1.5"></div>
