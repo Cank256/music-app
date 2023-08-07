d907a4b248e92cf2177e374b6ab010338fe7e1fc
Caleb Nkunze <nkunzecaleb@gmail.com>
2023-08-07 13:28:45 +0300

    Comment on the Web and Auth Routes

--- routes/auth.php
+++ routes/auth.php
@@ -11,54 +11,56 @@ use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
+// GUEST MIDDLEWARE ROUTES
+// Handles routes available to unauthenticated users (guests)
 Route::middleware('guest')->group(function () {
-    Route::get('signup', [RegisteredUserController::class, 'create'])
-                ->name('signup');
 
+    // SIGNUP ROUTES
+    // Displays the registration form and handles registration
+    Route::get('signup', [RegisteredUserController::class, 'create'])->name('signup');
     Route::post('signup', [RegisteredUserController::class, 'store']);
 
-    Route::get('login', [AuthenticatedSessionController::class, 'create'])
-                ->name('login');
-
+    // LOGIN ROUTES
+    // Displays the login form and handles authentication
+    Route::get('login', [AuthenticatedSessionController::class, 'create'])->name('login');
     Route::post('login', [AuthenticatedSessionController::class, 'store']);
 
-    Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])
-                ->name('password.request');
+    // FORGOT PASSWORD ROUTES
+    // Handles forgotten password requests and email notifications
+    Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])->name('password.request');
+    Route::post('forgot-password', [PasswordResetLinkController::class, 'store'])->name('password.email');
 
-    Route::post('forgot-password', [PasswordResetLinkController::class, 'store'])
-                ->name('password.email');
+    // RESET PASSWORD ROUTES
+    // Handles password reset functionality
+    Route::get('reset-password/{token}', [NewPasswordController::class, 'create'])->name('password.reset');
+    Route::post('reset-password', [NewPasswordController::class, 'store'])->name('password.store');
 
-    Route::get('reset-password/{token}', [NewPasswordController::class, 'create'])
-                ->name('password.reset');
-
-    Route::post('reset-password', [NewPasswordController::class, 'store'])
-                ->name('password.store');
-
-    Route::get('auth/google', [AuthenticatedSessionController::class, 'signInwithGoogle'])
-                ->name('google.signin');
-
+    // GOOGLE AUTHENTICATION ROUTES
+    // Handles authentication via Google OAuth
+    Route::get('auth/google', [AuthenticatedSessionController::class, 'signInwithGoogle'])->name('google.signin');
     Route::get('auth/google/callback', [AuthenticatedSessionController::class, 'callbackToGoogle']);
 });
 
+// AUTH MIDDLEWARE ROUTES
+// Handles routes available to authenticated users
 Route::middleware('auth')->group(function () {
-    Route::get('verify-email', EmailVerificationPromptController::class)
-                ->name('verification.notice');
 
-    Route::get('verify-email/{id}/{hash}', VerifyEmailController::class)
-                ->middleware(['signed', 'throttle:6,1'])
-                ->name('verification.verify');
+    // EMAIL VERIFICATION ROUTES
+    // Handles email verification process
+    Route::get('verify-email', EmailVerificationPromptController::class)->name('verification.notice');
+    Route::get('verify-email/{id}/{hash}', VerifyEmailController::class)->middleware(['signed', 'throttle:6,1'])->name('verification.verify');
+    Route::post('email/verification-notification', [EmailVerificationNotificationController::class, 'store'])->middleware('throttle:6,1')->name('verification.send');
 
-    Route::post('email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
-                ->middleware('throttle:6,1')
-                ->name('verification.send');
-
-    Route::get('confirm-password', [ConfirmablePasswordController::class, 'show'])
-                ->name('password.confirm');
-
+    // CONFIRM PASSWORD ROUTES
+    // Handles password confirmation for extra security
+    Route::get('confirm-password', [ConfirmablePasswordController::class, 'show'])->name('password.confirm');
     Route::post('confirm-password', [ConfirmablePasswordController::class, 'store']);
 
+    // UPDATE PASSWORD ROUTE
+    // Handles password updates
     Route::put('password', [PasswordController::class, 'update'])->name('password.update');
 
-    Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])
-                ->name('logout');
+    // LOGOUT ROUTE
+    // Handles user logout
+    Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
 });
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,12 @@
 <?php
 
 use App\Http\Controllers\ArtistController;
+use App\Http\Controllers\AlbumController;
 use App\Http\Controllers\BrowseController;
 use App\Http\Controllers\FavoriteController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\SearchController;
-use App\Http\Controllers\AlbumController;
 use App\Http\Controllers\SongController;
 use Illuminate\Support\Facades\Route;
 
@@ -21,38 +21,52 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// HOME ROUTE
+// Handles the landing page for the application
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
-//SEARCH ROUTES
+// SEARCH ROUTES
+// Handles the creation and execution of searches
 Route::get('/search', [SearchController::class, 'create'])->name('search');
 Route::get('/searching', [SearchController::class, 'search'])->name('searching');
 
+// ARTIST, ALBUM, AND SONG ROUTES
+// Retrieve information about artists, albums, and songs
 Route::get('/artist', [ArtistController::class, 'getArtist'])->name('get-artist');
 Route::get('/album', [AlbumController::class, 'getAlbum'])->name('get-album');
 Route::get('/song/{mbid}', [SongController::class, 'getSong'])->name('get-song');
 
-//BROWSE ROUTES
+// BROWSE ROUTES
+// Browse various tags and categories
 Route::get('/browse', [BrowseController::class, 'index'])->name('browse');
 Route::get('/tag', [BrowseController::class, 'getTag'])->name('get-tag');
 
-//TRACK/SONG ROUTES
+// TRACK/SONG ROUTES
+// Retrieve specific details about tracks or songs
 Route::get('/track-duration', [SongController::class, 'getTrackDuration'])->name('track-duration');
 
-//AUTHENTICATED USER ROUTES
+// AUTHENTICATED USER ROUTES
+// Routes that require user authentication
 Route::middleware('auth')->group(function () {
 
+    // DASHBOARD AND PROFILE ROUTES
+    // Handles user dashboard and profile management
     Route::get('/dashboard', [HomeController::class, 'dashboard'])->middleware(['verified'])->name('dashboard');
     Route::get('/profile', [ProfileController::class, 'index'])->name('profile');
     Route::get('/profile-edit', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    //LIBRARY ROUTES
+    // LIBRARY ROUTES
+    // Access to user's personal library
     Route::get('/library', [FavoriteController::class, 'index'])->name('library');
 
-    //FAVORITES ROUTES
+    // FAVORITES ROUTES
+    // Add and remove favorites
     Route::post('/favorite', [FavoriteController::class, 'add'])->name('add-favorite');
     Route::delete('/favorite', [FavoriteController::class, 'remove'])->name('remove-favorite');
 });
 
+// AUTHENTICATION ROUTES
+// Include the additional authentication-related routes
 require __DIR__.'/auth.php';
